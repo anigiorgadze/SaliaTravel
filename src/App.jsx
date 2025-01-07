26f2b4c58b9b858b9beb56e1843a3ae15634625f
anigiorgadze <anuukigiorgadze@gmail.com>
2025-01-08 01:31:12 +0400

    0.0.6

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from "./Home";
 import About from "./About";
 import Contact from "./Contact";
 import ProductDetails from "./ProductDetails";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -13,16 +14,14 @@ function App() {
     <Router>
       <Header />
       <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/Home" element={<Home />}/>
-          <Route path="/About" element={<About />}/>
-          <Route path="/Contact" element={<Contact />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
 
-          <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
       </Routes>
-        <div style={{position: "relative"}}> 
-          <h1>footer</h1>
-        </div>
+      <Footer />
 
     </Router>
     // <div>
--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,133 +1,78 @@
-// import pin from "./assets/pin.png"
-// import mail from "./assets/mail.png"
-// import phone from "./assets/telephone.png"
-
-// import img1 from './assets/country/czech.jpg'
-// import connect from './assets/country/connect.jpg'
-// // import connect from './assets/country/drop.avif'
-// import img2 from './assets/country/italy.webp'
-
-// import { useEffect } from "react"
-// import hoverEffect from "hover-effect";
-
-// import spain from "./assets/country/spain.jpg"
-// import egypt from "./assets/country/egypt.jpg"
-// import turkey from "./assets/country/turkey.jpg"
-// import italy from "./assets/country/italy.webp"
-// import czech from "./assets/country/czech.jpg"
-// import france from "./assets/country/france.webp"
-// import india from "./assets/country/india.jpg"
-// import thailand from "./assets/country/thailand.jpg"
-
-// const imgList = [
-//     {
-//         id:1,
-//         img: spain
-//     },
-//     {
-//         id:2,
-//         img: egypt
-//     },
-//     {
-//         id:3,
-//         img: turkey
-//     },
-//     {
-//         id:4,
-//         img: italy
-//     },
-//     {
-//         id:5,
-//         img: czech
-//     },
-//     {
-//         id:6,
-//         img: france
-//     },
-//     {
-//         id:7,
-//         img: india
-//     },
-//     {
-//         id:8,
-//         img: thailand
-//     },
-// ]
+import React from 'react'
+import arrow from './assets/arrow-right.svg'
+import call from './assets/call-1.svg'
+import location from './assets/location-dot2.svg'
+import mail from './assets/mail-1.svg'
+import btn from './assets/plane.svg'
+// import { CiMail } from "react-icons/ci";
 
 function Contact() {
-
     return (
-        <div className="contact-container">
-            {/* <div className="bg-slider">
-                <div className="slider-track">
-                    {
-                        imgList.map(item => (
-                            <img key={item.id} src={item.img} alt="image" />
-                        ))
-                    }
+        <div className='contact'>
+            <div className='contact__bg'>
+                <h2>საკონტაქტო</h2>
+                <div>
+                    <a href="/home">მთავარი</a>
+                    <img src={arrow} alt="arrow" />
+                    <p>საკონტაქტო</p>
                 </div>
+            </div>
 
-                <div className="over-color">
-                </div>
-            </div> */}
-            <div className="bg"></div>
-            <div className="contact">
-                <div className="head">
-                    <h2>საკონტაქტო</h2>
-                    <p><a href="/home">მთავარი</a> / საკონტაქტო</p>
-                </div>
+            <div className='contact__main'>
+                <h3>ჩვენი საკონტაქტო ინფორმაცია</h3>
 
-                <div className="contact-row">
-                    <section>
-                        <div className="row">
-                            <div className="icon">
-                                <ion-icon name="call-outline"></ion-icon>
-                            </div>
-                            <div className="text">
-                                <p>ტელეფონი:</p>
-                                <a href="tel:+995557171706">+995 557 17 17 06</a>
+                <div className='contact__info'>
+                    <a href="https://www.google.com/maps/place/2+Giorgi+Leonidze+St,+T'bilisi/@41.6927389,44.7981132,17z/data=!3m1!4b1!4m6!3m5!1s0x40440cee48649a3f:0x825ebef640cfd9fc!8m2!3d41.6927349!4d44.8006881!16s%2Fg%2F11_txht4q?entry=ttu&g_ep=EgoyMDI1MDEwNi4wIKXMDSoASAFQAw%3D%3D" target='blank'>
+                        <div className='contact__item'>
+                            <div className='img__bg'><img src={location} alt="" /></div>
+                            <div>
+                                <h4>მისამართი:</h4>
+                                <p>გ. ლეონიძის ქ. #2, თბილისი</p>
                             </div>
                         </div>
-                        <div className="row">
-                            <div className="icon">
-                                <ion-icon name="location-outline"></ion-icon>
-                            </div>
-                            <div className="text">
-                                <p>მისამართი:</p>
-                                <p>თბილისი, გიორგი ლეონიძის 2 </p>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="icon">
-                                <ion-icon name="mail-outline"></ion-icon>
-                            </div>
-                            <div className="text">
-                                <p>იმეილი:</p>
-                                <p>saliatravel@gmail.com</p>
+                    </a>
+
+                    <a href="tel:+995557171706">
+                        <div className='contact__item'>
+                            <div className='img__bg'><img src={call} alt="" /></div>
+                            <div>
+                                <h4>ტელეფონი:</h4>
+                                <p>+995 557 171 706</p>
                             </div>
                         </div>
-                    </section>
-                    <div>
-                        <b>ან მოგვწერეთ სოციალურ ქსელებში:</b>
-                        <div className="soc-network">
-                            <a href="https://www.facebook.com/profile.php?id=100076234376477" target='blank'><ion-icon name="logo-facebook"></ion-icon>Facebook</a> 
-                            <a href="https://www.instagram.com/saliatravel/" target='blank'><ion-icon name="logo-instagram"></ion-icon>Instagram</a>
-                            <a href="https://www.tiktok.com/@saliatravel?_t=8sR5boVvKbd&_r=1" target='blank'><ion-icon name="logo-tiktok"></ion-icon>TikTok</a>  
+                    </a>
+
+                    <a href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSBmzlZmbtZPPHKdRSncsqpqwkxDdkqZvdvJhhtgrKVjGWgmFBvfLPpGFRWmdPVjbjSnNdnC" target='blank'>
+                        <div className='contact__item'>
+                            <div className='img__bg'><img src={mail} alt="" /></div>
+                            <div>
+                                <h4>მეილი:</h4>
+                                <p>lannasalia@gmail.com</p>
+                            </div>
                         </div>
-                    </div>
+                    </a>
                 </div>
-                <div className="layout">
+            </div>
+
+            <div className='form__bg'>
+                <div className='user__info'>
                     <form>
-                        <input type="text" placeholder="სახელი" />
-                        <input type="email" name="" placeholder="eMmil" id="" />
-                        <input type="text"placeholder="თემა" />
-                        <textarea name="" placeholder="თექსტი" id=""></textarea>
-                        <button>გაგზავნა</button>
-
+                        <h3>დაგვიტოვეთ თქვენი საკონტაქტო ინფორმაცია</h3>
+                        <div>
+                            <input type="text" placeholder='სახელი' />
+                            <input type="text" placeholder='ტელეფონი' />
+                        </div>
+                        <input type="text" placeholder='თემა' />
+                        <textarea placeholder='დაგვიტოვეთ შეტყობინება...'></textarea>
+                        <button>გაგზავნა <img src={btn} alt="" /></button>
                     </form>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d885.7475923490256!2d44.80018490103581!3d41.69272020285425!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40440cee48649a3f%3A0x825ebef640cfd9fc!2s2%20Giorgi%20Leonidze%20St%2C%20T&#39;bilisi!5e0!3m2!1sen!2sge!4v1735634624945!5m2!1sen!2sge" allowfullscreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                 </div>
-            </div> 
+            </div>
+
+            <div className='map'>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d372.41141563818724!2d44.800323319571824!3d41.69264627313263!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40440cee48649a3f%3A0x825ebef640cfd9fc!2s2%20Giorgi%20Leonidze%20St%2C%20T&#39;bilisi!5e0!3m2!1sen!2sge!4v1736283176375!5m2!1sen!2sge"  allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+
         </div>
     )
 }
